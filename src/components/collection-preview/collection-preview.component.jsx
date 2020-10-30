2ec82de63606776d196e4afb6cbb007b8ad660a2
satyam95 <satyamsagar30@gmail.com>
2020-10-30 11:39:45 +0530

    Added two new components collection-perview ans collection-item in shoppage

---
+++ src/components/collection-preview/collection-preview.component.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+import '../../components/collection-item/collection-item.component';
+import CollectionItem from '../../components/collection-item/collection-item.component';
+
+import './collection-preview.style.scss';
+
+const CollectionPreview = ({title, items}) =>(
+    <div className='collection-preview'>
+        <h1 className='title'>
+            {title.toUpperCase()}
+        </h1>
+        <div className='preview'>
+            {items
+               .filter((item, idx) => idx < 4)
+               .map(({ id, ...otherItemProps }) => (
+                 <CollectionItem key={id} {...otherItemProps}></CollectionItem>
+               ))
+            }
+        </div>
+    </div>
+);
+
+export default CollectionPreview;
