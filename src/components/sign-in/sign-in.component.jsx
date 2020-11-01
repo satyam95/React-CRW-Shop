d241f6a6b2c5eae5e7e9d3e0f8cf21514771bdfd
satyam95 <satyamsagar30@gmail.com>
2020-11-01 21:20:19 +0530

    finished signin signup completely

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -4,7 +4,7 @@ import '../../components/form-input/form-input.component';
 import FormInput from '../../components/form-input/form-input.component';
 import CustomButton from '../custom-button/custom-button.component';
 
-import { signInWithGoogle} from '../../firebase/firebase.utils';
+import { auth, signInWithGoogle} from '../../firebase/firebase.utils';
 
 import './sign-in.style.scss';
 
@@ -18,11 +18,18 @@ class SignIn extends React.Component{
         }
     }
 
-    handleSubmit = event => {
+    handleSubmit = async event => {
         event.preventDefault();
+        
+        const { email, password } = this.state;
 
-        this.setState({email: '', password: ''})
-    }
+        try{
+            await auth.signInWithEmailAndPassword(email, password);
+            this.setState({email: '', password: ''});
+        } catch (error){
+            console.log(error);
+        }
+    };
 
     handleChange = event => {
         const { value, name } = event.target;
